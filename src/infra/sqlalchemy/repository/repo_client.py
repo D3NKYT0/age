5904dea0acc5d82c0138591cff9337620c5b025d
Renan Nere <renan.nere@outlook.com>
2022-12-28 15:55:50 -0300

    repositorio clientes

--- src/infra/sqlalchemy/repository/repo_client.py
+++ src/infra/sqlalchemy/repository/repo_client.py
@@ -1,30 +1,41 @@
 from sqlalchemy import select, delete, update
 from sqlalchemy.orm import Session
 
-from src.schemas import schemas_users
-from src.infra.sqlalchemy.models import models_user
+from src.schemas import schemas_client
+from src.infra.sqlalchemy.models import models_client
 
 
-class RepositoryUser():
+class RepositoryClient():
     
     def __init__(self, db: Session):
         self.db = db
 
     def searchById(self, id: int):
-        query = select(schemas_users.User).where(schemas_users.User.id == id)
-        user = self.db.execute(query).first()
-        return user
+        query = select(schemas_client.Client).where(schemas_client.Client.id == id)
+        Client = self.db.execute(query).first()
+        return Client
 
-    def register(self, user: schemas_users.User):
+    def register_client(self, client: schemas_client.Client):
 
         # conversao do schema em model
-        db_user = models_user.User(
-            name = user.name,
-            create_at = user.create_at,
-            login = user.login,
-            password = user.password,
-            email = user.email,
-            classified_as = user.classified_as
+        db_user = models_client.Client(
+            create_at = client.create_at,
+            birth_date = client.birth_date,
+            name = client.name,
+            cep = client.cep,
+            UF = client.UF,
+            city = client.city,
+            address = client.address,
+            number = client.number,
+            complement = client.complement,
+            phone = client.phone,
+            line_of_credit = client.line_of_credit,
+            line_of_business = client.line_of_business,
+            start_of_business = client.start_of_business,
+            staus_id = client.status_id,
+            lse_id = client.lse_id,
+            user_id = client.user_id
+
         )
 
         # operações no banco de dados
@@ -34,31 +45,41 @@ class RepositoryUser():
 
         return db_user
 
-    def edit(self, user_id: int, user: schemas_users.User):
-            update_statement = update(models_user.User).where(
-                models_user.User.id == user_id
+    def edit_client(self, client_id: int, client: schemas_client.Client):
+            update_statement = update(models_client.Client).where(
+                models_client.Client.id == client_id
             ).values(
-                name = user.name,
-                login = user.login,
-                password = user.password,
-                email = user.email,
-                classified_as = user.classified_as
+                create_at = client.create_at,
+                birth_date = client.birth_date,
+                cep = client.cep,
+                UF = client.UF,
+                city = client.city,
+                address = client.address,
+                number = client.number,
+                complement = client.complement,
+                phone = client.phone,
+                line_of_credit = client.line_of_credit,
+                line_of_business = client.line_of_business,
+                start_of_business = client.start_of_business,
+                status_id = client.status_id,
+                lse_id = client.lse_id,
+                user_id = client.user_id
             )
 
             self.db.execute(update_statement)
             self.db.commit()
-            return user
+            return client_id
 
-    def show_all_users(self):
-        users = self.db.query(schemas_users.User).all()
-        return users
+    def show_all_clients(self):
+        client = self.db.query(schemas_client.Client).all()
+        return client
 
-    def remove(self, user_id: int):
-        statement = select(schemas_users.User).filter_by(id=user_id)
-        user = self.db.execute(statement).first()
+    def remove(self, client_id: int):
+        statement = select(schemas_client.Client).filter_by(id=client_id)
+        client_id = self.db.execute(statement).first()
 
-        statement = delete(schemas_users.User).where(schemas_users.User.id == user_id)
+        statement = delete(schemas_client.Client).where(schemas_client.Client.id == client_id)
         self.db.execute(statement)
         self.db.commit()
 
-        return user
+        return client_id
