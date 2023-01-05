8468699f9d0c9317336bf723f0acb6a0d6e87d9a
Renan Nere <renan.nere@outlook.com>
2023-01-05 16:05:16 -0300

    ajuste dos routers authorization and classifier user

--- src/routers/router_authorization.py
+++ src/routers/router_authorization.py
@@ -2,6 +2,8 @@ from fastapi import Depends, status, APIRouter, HTTPException
 from fastapi_limiter.depends import RateLimiter
 from src.resources.utils import add_create_at_timestamp
 
+from typing import List
+
 from sqlalchemy.orm import Session
 from src.infra.sqlalchemy.config.database import get_db
 from src.resources.auth_utils import get_user_logged
@@ -13,6 +15,34 @@ from src.infra.sqlalchemy.repository.repo_authorization import RepositoryAuthori
 
 
 router = APIRouter()
+
+@router.get('/get/{id}', status_code=status.HTTP_200_OK, response_model=schemas_authorization.Authorization, tags=["authorizations"])
+def show_authorization(id: int, _ = Depends(get_user_logged), db: Session = Depends(get_db)):
+
+    if not check_authorization(db, ["root"]):
+        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="You do not have authorization to access!")
+
+    authorization_located = add_create_at_timestamp(authorization_located)
+
+    authorization_located = RepositoryAuthorization(db).searchById(id)
+
+    if not authorization_located:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Authorization not exist!")
+
+    return authorization_located
+
+@router.get('/get/all', status_code=status.HTTP_200_OK, response_model=List[schemas_authorization.Authorization], tags=["authorizations"])
+def show_all_authorization( _ = Depends(get_user_logged), db: Session = Depends(get_db)):
+
+    if not check_authorization(db, ["root"]):
+        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="You do not have authorization to access!")
+
+    all_authorization = RepositoryAuthorization(db).show_all_authorization()
+
+    if not all_authorization:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="There are no authorizations located!")
+
+    return all_authorization
 
 @router.post('/authorization', status_code=status.HTTP_201_CREATED, response_model=schemas_authorization.Authorization, dependencies=[Depends(RateLimiter(times=2, seconds=5))], tags=["authorizations"])
 def register_auth(auth_data: schemas_authorization.Authorization, _ = Depends(get_user_logged), db: Session = Depends(get_db)):
@@ -30,3 +60,31 @@ def register_auth(auth_data: schemas_authorization.Authorization, _ = Depends(ge
     authorization_created = RepositoryAuthorization(db).register_authorization(auth_data)
 
     return authorization_created
+
+@router.put('/update/{id}', status_code=status.HTTP_200_OK, response_model=schemas_authorization.Authorization, tags=["authorizations"])
+def update_authorization(id: int, authorization: schemas_authorization.Authorization, _ = Depends(get_user_logged), db: Session = Depends(get_db)):
+
+    if not check_authorization(db, _, ["root"]):
+        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="You do not have authorization to access!")
+
+    authorization = add_create_at_timestamp(authorization, True)
+
+    # -- register log here --
+    # --
+    # -- end --
+
+    authorization_updated = RepositoryAuthorization(db).edit_authorization(id, authorization)
+    authorization_updated.id = id
+
+    return authorization_updated
+
+@router.delete('/delete/{id}', status_code=status.HTTP_200_OK, response_model=schemas_authorization.Authorization, tags=["authorizations"])
+def delete_authorization(authorization_id: int, _ = Depends(get_user_logged) ,db: Session = Depends(get_db)):
+
+    if not check_authorization(db, _, ["root"]):
+        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="You do not have authorization to access!")
+
+    authorization = add_create_at_timestamp(authorization, True)
+
+    authorization = RepositoryAuthorization(db).remove_authorization(authorization_id)
+    return authorization
--- src/routers/router_classifier_user.py
+++ src/routers/router_classifier_user.py
@@ -2,6 +2,8 @@ from fastapi import Depends, status, APIRouter, HTTPException
 from fastapi_limiter.depends import RateLimiter
 from src.resources.utils import add_create_at_timestamp
 
+from typing import List
+
 from sqlalchemy.orm import Session
 from src.infra.sqlalchemy.config.database import get_db
 from src.resources.auth_utils import get_user_logged
@@ -13,6 +15,33 @@ from src.infra.sqlalchemy.repository.repo_classifier_user import RepositoryClass
 
 router = APIRouter()
 
+@router.get('/get/{id}', status_code=status.HTTP_200_OK, response_model=schemas_classifier_user.ClassifierUser, tags=["classifier_users"])
+def show_classifier_user(id: int, _ = Depends(get_user_logged), db: Session = Depends(get_db)):
+
+    if not check_authorization(db, ["root"]):
+        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="You do not have authorization to access!")
+
+    classifier_user_located = add_create_at_timestamp(classifier_user_located)
+
+    classifier_user_located = RepositoryClassifierUser(db).searchById(id)
+
+    if not classifier_user_located:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Classifier user not exist!")
+
+    return classifier_user_located
+
+@router.get('/get/all', status_code=status.HTTP_200_OK, response_model=List[schemas_classifier_user.ClassifierUser], tags=["classifier_users"])
+def show_all_classifier_user( _ = Depends(get_user_logged), db: Session = Depends(get_db)):
+
+    if not check_authorization(db, ["root"]):
+        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="You do not have authorization to access!")
+
+    all_classifier_user = RepositoryClassifierUser(db).show_all_classifier_users()
+
+    if not all_classifier_user:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="There are no classifier users located!")
+
+    return all_classifier_user 
 
 @router.post('/classifier', status_code=status.HTTP_201_CREATED, response_model=schemas_classifier_user.ClassifierUser, dependencies=[Depends(RateLimiter(times=2, seconds=5))], tags=["classifier_users"])
 def register_classifier(classifier_data: schemas_classifier_user.ClassifierUser, _ = Depends(get_user_logged), db: Session = Depends(get_db)):
@@ -22,7 +51,7 @@ def register_classifier(classifier_data: schemas_classifier_user.ClassifierUser,
 
     classifier_data = add_create_at_timestamp(classifier_data)
 
-    exist_auth = RepositoryAuthorization(db).searchById(classifier_data.authorization_id)
+    exist_auth = RepositoryClassifierUser(db).searchById(classifier_data.authorization_id)
 
     if not exist_auth:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Althorization not found!")
@@ -35,3 +64,31 @@ def register_classifier(classifier_data: schemas_classifier_user.ClassifierUser,
     classifier_created = RepositoryClassifierUser(db).register_classifier_user(classifier_data)
 
     return classifier_created
+
+@router.put('/update/{id}', status_code=status.HTTP_200_OK, response_model=schemas_classifier_user.ClassifierUser, tags=["classifier_users"])
+def update_classifier_user(id: int, classifier_user: schemas_classifier_user.ClassifierUser, _ = Depends(get_user_logged), db: Session = Depends(get_db)):
+
+    if not check_authorization(db, _, ["root"]):
+        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="You do not have authorization to access!")
+
+    classifier_user = add_create_at_timestamp(classifier_user, True)
+
+    # -- register log here --
+    # --
+    # -- end --
+
+    classifier_user_updated= RepositoryClassifierUser(db).edit_classifier_user(id, classifier_user)
+    classifier_user_updated.id = id
+
+    return classifier_user_updated
+
+@router.delete('/delete/{id}', status_code=status.HTTP_200_OK, response_model=schemas_classifier_user.ClassifierUser, tags=["classifier_user"])
+def delete_classifier_user(classifier_user_id: int, _ = Depends(get_user_logged) ,db: Session = Depends(get_db)):
+
+    if not check_authorization(db, _, ["root"]):
+        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="You do not have authorization to access!")
+
+    classifier_user = add_create_at_timestamp(classifier_user, True)
+
+    classifier_user = RepositoryClassifierUser(db).remove_classifier_user(classifier_user_id)
+    return classifier_user
