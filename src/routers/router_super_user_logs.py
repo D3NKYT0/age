5d7838d657c4afb7758e536d1ba5e8e143b21080
Renan Nere <renan.nere@outlook.com>
2023-01-05 12:42:36 -0300

    segundo ajuste de rota

--- src/routers/router_super_user_logs.py
+++ src/routers/router_super_user_logs.py
@@ -7,7 +7,7 @@ from src.resources.auth_utils import get_user_logged
 from src.resources.utils import check_authorization
 from src.resources.utils import add_create_at_timestamp
 
-from src.schemas import schemas_super_users_logs
+from src.schemas import schemas_super_user_logs
 from src.infra.sqlalchemy.repository.repo_super_user_logs import RepositorySuperUserLogs
 from src.infra.sqlalchemy.config.database import get_db
 
@@ -15,7 +15,7 @@ from src.infra.sqlalchemy.config.database import get_db
 router = APIRouter()
 
 
-@router.get('/get/{id}', status_code=status.HTTP_200_OK, response_model=schemas_super_users_logs.SimpleSuperUserLogs, tags=["super_users_logs"])
+@router.get('/get/{id}', status_code=status.HTTP_200_OK, response_model=schemas_super_user_logs.SuperUserLogs, tags=["super_users_logs"])
 def show_super_user_logs(id: int, _ = Depends(get_user_logged), db: Session = Depends(get_db)):
 
     if not check_authorization(db, ["root"]):
@@ -30,7 +30,7 @@ def show_super_user_logs(id: int, _ = Depends(get_user_logged), db: Session = De
 
     return logs_super_user_located
 
-@router.get('/get/all', status_code=status.HTTP_200_OK, response_model=List[schemas_super_users_logs.SimpleSuperUserLogs], tags=["super_users_logs"])
+@router.get('/get/all', status_code=status.HTTP_200_OK, response_model=List[schemas_super_user_logs.SuperUserLogs], tags=["super_users_logs"])
 def show_super_user_logs( _ = Depends(get_user_logged), db: Session = Depends(get_db)):
 
     if not check_authorization(db, ["root"]):
@@ -43,8 +43,8 @@ def show_super_user_logs( _ = Depends(get_user_logged), db: Session = Depends(ge
 
     return all_logs_super_users
 
-@router.post('/register/', status_code=status.HTTP_201_CREATED, response_model=schemas_super_users_logs.SimpleSuperUserLogs, tags=["super_users_logs"])
-def create_Logs_super_user(logs_super_user: schemas_super_users_logs.SuperUserLogs, _ = Depends(get_user_logged), db: Session = Depends(get_db)):
+@router.post('/register/', status_code=status.HTTP_201_CREATED, response_model=schemas_super_user_logs.SuperUserLogs, tags=["super_users_logs"])
+def create_Logs_super_user(logs_super_user: schemas_super_user_logs.SuperUserLogs, _ = Depends(get_user_logged), db: Session = Depends(get_db)):
 
     if not check_authorization(db, _, ["root"]):
         raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="You do not have authorization to access!")
@@ -54,8 +54,8 @@ def create_Logs_super_user(logs_super_user: schemas_super_users_logs.SuperUserLo
     logs_super_user_created = RepositorySuperUserLogs(db).register(logs_super_user)
     return logs_super_user_created
 
-@router.put('/update/{id}', status_code=status.HTTP_200_OK, response_model=schemas_super_users_logs.SimpleSuperUserLogs, tags=["super_users_logs"])
-def update_super_users_logs(id: int, super_user_logs: schemas_super_users_logs.SuperUser, _ = Depends(get_user_logged), db: Session = Depends(get_db)):
+@router.put('/update/{id}', status_code=status.HTTP_200_OK, response_model=schemas_super_user_logs.SuperUserLogs, tags=["super_users_logs"])
+def update_super_users_logs(id: int, super_user_logs: schemas_super_user_logs.SuperUserLogs, _ = Depends(get_user_logged), db: Session = Depends(get_db)):
 
     if not check_authorization(db, _, ["root"]):
         raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="You do not have authorization to access!")
@@ -71,7 +71,7 @@ def update_super_users_logs(id: int, super_user_logs: schemas_super_users_logs.S
 
     return logs_super_user_updated
 
-@router.delete('/delete/{id}', status_code=status.HTTP_200_OK, response_model=schemas_super_users_logs.SimpleSuperUserLogs, tags=["super_users_logs"])
+@router.delete('/delete/{id}', status_code=status.HTTP_200_OK, response_model=schemas_super_user_logs.SuperUserLogs, tags=["super_users_logs"])
 def delete_logs_super_users(logs_super_user_id: int, _ = Depends(get_user_logged) ,db: Session = Depends(get_db)):
 
     if not check_authorization(db, _, ["root"]):
